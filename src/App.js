efd357c6c49eda573b9642300ad40cf87127cc74
jlasgithub <da33@njit.edu>
2024-08-26 19:03:07 +0000

    navbar init with styling

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import {useNavigate, seState, useEffect} from "react";
 import Search from './screens/Search';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/Universal/NavBar';
 
 function App() {
   return (
     <div className = "container-fluid mb-5">
-
-    
-    <Router>
-    <div className="App">
-      <Routes>
-      <Route path="/search" element={<Search />} />
-      </Routes>
-    </div>
-    </Router>
+      
+      <Router>
+      <NavBar/>
+      <div className="App">
+        <Routes>
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </div>
+      </Router>
     </div>
   );
 }
