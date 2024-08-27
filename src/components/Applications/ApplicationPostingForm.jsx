b48011fa1e38209d6e3a9f3d27c5e63760e505a8
JacTobias <jactobias63@gmail.com>
2024-08-27 15:59:27 +0000

    initial front end for job application posting form

--- src/components/Applications/ApplicationPostingForm.jsx
+++ src/components/Applications/ApplicationPostingForm.jsx
@@ -2,26 +2,26 @@ import react from 'react';
 import {useState} from 'react';
 
 const ApplicationPostingForm = () => {
-    const [candidateId, setCandidateId] = useState('');
-    const onCandidateIdChange = (e) => setCandidateId(e);
-
-    const [jobId, setJobId] = useState('');
-    const onJobIdChange = (e) => setJobId(e);
-
     const [dateApplied, setDateApplied] = useState('');
     const onDateAppliedChange = (e) => setDateApplied(e);
 
-    const [coverLetter, setCoverLetter] = useState('');
+    const [candidateCoverLetter, setCoverLetter] = useState('');
     const onCoverLetterChange = (e) => setCoverLetter(e);
 
-    const [resume, setResume] = useState('');
+    const [candidateResume, setResume] = useState('');
     const onResumeChange = (e) => setResume(e);
 
     const [candidateName, setCandidateName] = useState('')
     const onCandidateNameChange = (e) => setCandidateName(e);
 
-    const [email, setEmail] = useState('');
+    const [candidateEmail, setEmail] = useState('');
     const onEmailChange = (e) => setEmail(e);
+
+    const [candidateAddress, setCandidateAddress] = useState('');
+    const onCandidateAddressChange = (e) => setCandidateAddress(e);
+
+    const [candidatePhone, setCandidatePhone] = useState('');
+    const onCandidatePhoneChange = (e) => setCandidatePhone(e);
 
 
                                                                             
@@ -32,8 +32,34 @@ const ApplicationPostingForm = () => {
         </div>
         <div className='col-6'>
             <div className='mt-3'>
-                <label htmlFor='setCandidateName'>Candidate Name</label>
-                <input type="text" className='form-control' id="inputCandidateName" ></input>
+                <label htmlFor='setCandidateName'>Candidate Name:</label>
+                <input type="text" className='form-control' id="inputCandidateName" placeholder="Enter Name"
+                value={candidateName} onChange={setCandidateName}></input>
+            </div>
+            <div className='mt-3'>
+                <label htmlFor='setEmail'>Candidate Email:</label>
+                <input type='text' className='form-control' id='inputEmail' placeholder="Enter Email"
+                 value={candidateEmail} onChange={setEmail}></input>
+            </div>
+            <div>
+                <label htmlFor='candidateAddress'>Candidate Address:</label>
+                <input type='text' className='form-control' id='inputAddress' placeholder="Enter Address"
+                value={candidateAddress} onChange={(setCandidateAddress)}></input>
+            </div>
+            <div>
+                <label htmlFor='candidatePhone'>Candidate Phone:</label>
+                <input type='text' className='form-control' id='inputPhone' placeholder="Enter Phone Number"
+                value={candidatePhone} onChange={(setCandidatePhone)}></input>
+            </div>
+            <div>
+                <label htmlFor='candidateCoverLetter'>Candidate Cover Letter</label>
+                <input type='text' className='form-control' id='inputCoverLetter' placeholder="Cover Letter"
+                value={candidateCoverLetter} onChange={(onCoverLetterChange)}></input>
+            </div>
+            <div>
+                <label htmlFor='candidateResume'>Candidate Resume</label>
+                <input type='text' className='form-control' id='inputResume' placeholder='Resume'
+                value={candidateResume} onChange={(onResumeChange)}></input>
             </div>
 
         </div>
