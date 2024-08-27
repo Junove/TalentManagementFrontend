8873bbaaac329935cb2f1efec7a4820beb6aa384
Xiyu-Serena-Wan <xiyuwanbjtu@gmail.com>
2024-08-27 16:05:21 +0000

    dashboard

--- src/screens/AdminDashboard.jsx
+++ src/screens/AdminDashboard.jsx
@@ -4,6 +4,23 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { Button, ButtonGroup } from '@mui/material';
 
+const buttonGroupStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    'flex-wrap': 'wrap',
+    boxShadow: 'none',
+  };
+
+
+const buttonStyle = {
+    textAlign: 'center',
+    '&:hover': {
+        backgroundColor: '#0056b3'
+    },
+    'margin-bottom': '1px',
+}
+
+
 const AdminDashboard = () => {
     return (
         <div className="container" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '90vh'}}>
@@ -15,13 +32,13 @@ const AdminDashboard = () => {
                         Welcome to the Admin Dashboard page! From this page, you can manage the following all of the data contained within this application. This includes but is not limited to the creation, deletion and updating of job postings, job applications, users, hiring managers, and administrators.
                     </p>
 
-                    <ButtonGroup variant="contained" aria-label="Admin Navigation Buttons">
-                        <Button href="/admin/administrator">Administrator</Button>
-                        <Button href="/admin/candidates">Candidates</Button>
-                        <Button href="/admin/hiringManagers">Hiring Managers</Button>
-                        <Button href="/admin/jobApplications">Job Applications</Button>
-                        <Button href="/admin/jobListings">Job Listings</Button>
-                        <Button href="/admin/users">Users</Button>
+                    <ButtonGroup variant="contained" aria-label="Admin Navigation Buttons" sx={buttonGroupStyle}>
+                        <Button sx={buttonStyle} href="/admin/administrator">Administrator</Button>
+                        <Button sx={buttonStyle} href="/admin/candidates">Candidates</Button>
+                        <Button sx={buttonStyle} href="/admin/hiringManagers">Hiring Managers</Button>
+                        <Button sx={buttonStyle} href="/admin/jobApplications">Job Applications</Button>
+                        <Button sx={buttonStyle} href="/admin/jobListings">Job Listings</Button>
+                        <Button sx={buttonStyle} href="/admin/users">Users</Button>
                     </ButtonGroup>
                 </CardContent>
             </Card>
