109c9d7aa8d3d5b5fef3773a0f03ab20b4567fbe
Ryan Quiogue <rquiogue17@gmail.com>
2024-08-27 15:49:29 -0400

    Added Validation

--- src/screens/Jobs/EditJobPosting.jsx
+++ src/screens/Jobs/EditJobPosting.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import JobEditingForm from '../../components/Jobs/JobEditingForm'
 import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
+import { LoginContext } from '../../components/Login/LoginContext';
 
 const EditJobPosting = () => {
   const { jobid } = useParams();
+  const { isLoggedIn, user, username, login, logout } = useContext(LoginContext);
+
+  
 
   return (
     <>
+    { user.type === 'hiring_manager' ? (
+      <>
         <div className='row'>
             <div className = "col-3">
 
@@ -18,6 +25,19 @@ const EditJobPosting = () => {
         
         <JobEditingForm jobId={jobid}/>
     </>
+    )
+    : (
+    <div className='row'>
+      <div className = "col-3">
+
+      </div>  
+      <div className = "col-6">
+        <h1 className='mx-auto text-start mt-3'>You are not a manager</h1>
+      </div>  
+    </div>
+    )
+    }
+  </>
   )
 }
 
