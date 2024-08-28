ed8a585a3364779518ff7706474e59309d1bd01b
Ryan Quiogue <rquiogue17@gmail.com>
2024-08-28 14:35:16 -0400

    Fixed Routing

--- src/Router/Routing.js
+++ src/Router/Routing.js
@@ -2,29 +2,29 @@ import { Route, Routes } from 'react-router-dom'
 
 import React from 'react'
 
-import ProfileWrapper from './components/Universal/ProfileWrapper.jsx';
-import JobDetails from './components/Jobs/JobDetails';
-import CandidateDashboard from './screens/CandidateDashboard';
-import ViewSingleApplication from './screens/Applications/ViewSingleApplication';
+import ProfileWrapper from '../components/Universal/ProfileWrapper.jsx';
+import JobDetails from '../components/Jobs/JobDetails';
+import CandidateDashboard from '../screens/CandidateDashboard';
+import ViewSingleApplication from '../screens/Applications/ViewSingleApplication';
 
-import AdminDashboard from './screens/AdminDashboard';
-import AdminManagement from './screens/Admin/AdminManagement';
-import CandidateManagement from './screens/Admin/CandidateManagement';
-import HiringManagerment from './screens/Admin/HiringManagerment';
-import JobApplicationManagement from './screens/Admin/JobApplicationManagement';
-import JobListingManagement from './screens/Admin/JobListingManagement';
-import UserManagement from './screens/Admin/UserManagement';
-import JobApplication from './screens/Applications/JobApplication';
-import CandidateRegister from './components/Register/CandidateRegister';
-import HiringManagerRegister from "./components/Register/HiringManagerRegister";
-import ManagerDashboard from './components/Manager/ManagerDashboard';
-import JobDetailView from './components/Manager/JobDetailView.jsx';
-import PageNotFound from './screens/PageNotFound';
-import Search from './screens/Search';
-import Login from './screens/Login';
-import CreateJobPosting from './screens/Jobs/CreateJobPosting';
-import EditJobPosting from './screens/Jobs/EditJobPosting';
-import UserRegister from './components/Register/UserRegister.jsx';
+import AdminDashboard from '../screens/AdminDashboard';
+import AdminManagement from '../screens/Admin/AdminManagement';
+import CandidateManagement from '../screens/Admin/CandidateManagement';
+import HiringManagerment from '../screens/Admin/HiringManagerment';
+import JobApplicationManagement from '../screens/Admin/JobApplicationManagement';
+import JobListingManagement from '../screens/Admin/JobListingManagement';
+import UserManagement from '../screens/Admin/UserManagement';
+import JobApplication from '../screens/Applications/JobApplication';
+import CandidateRegister from '../components/Register/CandidateRegister';
+import HiringManagerRegister from "../components/Register/HiringManagerRegister";
+import ManagerDashboard from '../components/Manager/ManagerDashboard';
+import JobDetailView from '../components/Manager/JobDetailView.jsx';
+import PageNotFound from '../screens/PageNotFound';
+import Search from '../screens/Search';
+import Login from '../screens/Login';
+import CreateJobPosting from '../screens/Jobs/CreateJobPosting';
+import EditJobPosting from '../screens/Jobs/EditJobPosting';
+import UserRegister from '../components/Register/UserRegister.jsx';
 import { Navigate } from 'react-router-dom';
 
 const Routing = (props) => {
