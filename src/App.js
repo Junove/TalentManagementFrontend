3b651035c0cbc19521f11ffde171814f823ac75e
Anchal Singh <anchal.singh@adp.com>
2024-08-29 15:04:30 +0000

    merge conflicts

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { useNavigate, useState, useEffect, useContext } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/Universal/NavBar';
-<<<<<<< HEAD
-
 import JobDetails from './components/Jobs/JobDetails';
 import CandidateDashboard from './screens/CandidateDashboard';
 import ViewSingleApplication from './screens/Applications/ViewSingleApplication';
@@ -15,7 +13,6 @@ import HiringManagerRegister from "./components/Register/HiringManagerRegister";
 import ManagerDashboard from './components/Manager/ManagerDashboard';
 import JobDetailView from './components/Manager/JobDetailView.jsx';
 import IndividualApplicationGridItem from './components/Candidate/IndividualApplicationGridItem.jsx';
-// Adjust the path as needed
 import Routing from './Router/Routing';
 import { LoginContext } from './components/Login/LoginContext';
 
